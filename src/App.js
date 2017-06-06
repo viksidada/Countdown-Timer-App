10e7279a4f7948715f523d943e23c8b72ecff091
vikas srivastava <vikas.abes@gmail.com>
2017-06-06 14:08:35 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React, {Component} from 'react';
 import "./App.css";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Deadline: "January 20, 2018"
+        }
+    }
+
     render() {
         return (
         <div className = "App">
-            <div className="App-title">My countdown application</div>
+            <div className="App-title">Countdown to {this.state.Deadline}</div><br/>
             <div>
-                <div>14 days</div>
-                <div>10 hours</div>
-                <div>23 minutes</div>
-                <div>21 seconds</div>
+                <div className="Clock-days">14 days</div>
+                <div className="Clock-hours">10 hours</div>
+                <div className="Clock-minutes">23 minutes</div>
+                <div className="Clock-seconds">21 seconds</div>
             </div>
             <div>
                 <input placeholder="new date" />
